b9805670f8531c7fc14428be0cdf2cf0e64172d3
Marcelo Morinigo <morinigomarcelo40@gmail.com>
2023-06-23 16:02:44 -0400

    tweaks to skills section

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -22,16 +22,16 @@ function Home() {
           </div>
           <p className='skills_title'>My <span>Abilities</span></p>
           <div className="skills">
-            <Skill icon={<DiHtml5/>} title={"Html"} percentaje={"85%"}/>
+            <Skill icon={<DiHtml5/>} title={"Html"} percentaje={"90%"}/>
             <Skill icon={<DiCss3/>} title={"Css"} percentaje={"90%"}/>
             <Skill icon={<DiJsBadge/>} title={"Javascript"} percentaje={"80%"}/>
             <Skill icon={<DiBootstrap/>} title={"Bootstrap"} percentaje={"60%"}/>
-            <Skill icon={<DiSass/>} title={"Sass"} percentaje={"65%"}/>
-            <Skill icon={<DiReact/>} title={"React"} percentaje={"75%"}/>
-            <Skill icon={<p><img src='/svg/ts.svg'/></p>} title={"Typescript"} percentaje={"55%"}/>
-            <Skill icon={<DiRuby/>} title={"Ruby"} percentaje={"60%"}/>
+            <Skill icon={<DiSass/>} title={"Sass"} percentaje={"60%"}/>
+            <Skill icon={<DiReact/>} title={"React"} percentaje={"70%"}/>
+            <Skill icon={<img src='/svg/ts.svg' alt='Typescript'/>} title={"Typescript"} percentaje={"60%"}/>
+            <Skill icon={<DiRuby/>} title={"Ruby"} percentaje={"70%"}/>
             <Skill icon={<DiRor/>} title={"Ruby on Rails"} percentaje={"75%"}/>
-            <Skill icon={<DiMysql/>} title={"Mysql"} percentaje={"50%"}/>
+            <Skill icon={<DiMysql/>} title={"Mysql"} percentaje={"70%"}/>
             <Skill icon={<DiGit/>} title={"Git"} percentaje={"70%"}/>
             <Skill icon={<DiLinux/>} title={"Linux"} percentaje={"80%"}/>
           </div>
