8aa3efd6537513aac177e4bba2e143ad07116f7d
MarceloM47 <morinigomarcelo40@gmail.com>
2023-10-27 22:23:44 -0300

    Se hizo el cambió de una imagen para la sección de proyectos

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,7 +1,7 @@
 import {BsRocketTakeoffFill} from 'react-icons/bs'
 import Project from '../components/Project'
 
-const task_list_bg = "/images/projects_image/task-list-bg.png";
+const lms_bg = "/images/projects_image/lms-bg.png";
 const coffe_shop_bg = "/images/projects_image/coffe-shop-bg.png";
 const marcelo_shoes_bg = "/images/projects_image/marcelo-shoes-bg.png";
 
@@ -15,7 +15,7 @@ function Projects() {
         </div>
         <p className='title_section'>My <span>Projects</span></p>
           <div className="projects">
-            <Project src={task_list_bg} url="https://task-list-rails.fly.dev/" alt='Fondo de una Task List en Ruby on Rails'/>
+            <Project src={lms_bg} url="https://github.com/MarceloM47/Library-MS" alt='Fondo de una Task List en Ruby on Rails'/>
             <Project src={coffe_shop_bg} url="https://coffee-shop-liard.vercel.app/" alt='Fondo de una Tienda de café'/>
             <Project src={marcelo_shoes_bg} url="https://marcelo-shoes.vercel.app/" alt='Fondo de una tienda de zapatos'/>
           </div>
