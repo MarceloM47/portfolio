04fd873a2d587b7ae15aa8df18144d670aef0399
MarceloM47 <morinigomarcelo40@gmail.com>
2023-12-01 10:18:16 -0300

    Se cambió un proyecto por el sitio de eventos

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -3,7 +3,7 @@ import Project from '../components/Project'
 
 const lms_bg = "/images/projects_image/lms-bg.png";
 const coffe_shop_bg = "/images/projects_image/coffe-shop-bg.png";
-const marcelo_shoes_bg = "/images/projects_image/marcelo-shoes-bg.png";
+const eventos_bg = "/images/projects_image/eventos-bg.png";
 
 function Projects() {
 
@@ -16,8 +16,8 @@ function Projects() {
         <p className='title_section'>My <span>Projects</span></p>
           <div className="projects">
             <Project src={lms_bg} url="https://github.com/MarceloM47/Library-MS" alt='Fondo de una Task List en Ruby on Rails'/>
+            <Project src={eventos_bg} url="https://eventos.marcelom47.online/" alt='Fondo de un sitio web de invitaciones digitales'/>
             <Project src={coffe_shop_bg} url="https://coffee-shop-liard.vercel.app/" alt='Fondo de una Tienda de café'/>
-            <Project src={marcelo_shoes_bg} url="https://marcelo-shoes.vercel.app/" alt='Fondo de una tienda de zapatos'/>
           </div>
     </section>
   )
