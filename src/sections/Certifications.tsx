8bdb81d788fef1b6cf0e3661afe41b424a9aaa7c
MarceloM47 <morinigomarcelo40@gmail.com>
2023-12-20 17:31:55 -0300

    Remove some certifications

--- src/sections/Certifications.tsx
+++ src/sections/Certifications.tsx
@@ -2,26 +2,20 @@ import { BsFillClipboard2Fill } from 'react-icons/bs';
 import CertificateSlider from '../components/CertificateSlider';
 import certificado1 from '/images/certifications/tecnico_informatica.png';
 import certificado2 from '/images/certifications/frontend-practico.png';
-import certificado3 from '/images/certifications/accesibilidad_web.png';
-import certificado4 from '/images/certifications/MSA.png';
-import certificado5 from '/images/certifications/css a fondo.png';
-import certificado6 from '/images/certifications/javascript y el dom.png';
-import certificado7 from '/images/certifications/algoritmos.png';
-import certificado8 from '/images/certifications/EDL.png';
-import certificado9 from '/images/certifications/Linux Cisco.png';
-import certificado10 from '/images/certifications/aguarandu2022.png';
-import certificado11 from '/images/certifications/economia digital.png';
-import certificado12 from '/images/certifications/github a fondo.png';
-import certificado13 from '/images/certifications/jekupyty domotica.png';
-import certificado14 from '/images/certifications/open robotics.png';
-import certificado15 from '/images/certifications/python profesional.png';
-import certificado16 from '/images/certifications/react-profesional.png';
-import certificado17 from '/images/certifications/redes.png';
-import certificado18 from '/images/certifications/ror.png';
-import certificado19 from '/images/certifications/rorDiploma.png';
-import certificado20 from '/images/certifications/desarrollo web profesional.png';
-import certificado21 from '/images/certifications/bootcamp-rails.png';
-import certificado22 from '/images/certifications/monitor_suena_san_juan.png';
+import certificado3 from '/images/certifications/MSA.png';
+import certificado4 from '/images/certifications/css a fondo.png';
+import certificado5 from '/images/certifications/javascript y el dom.png';
+import certificado6 from '/images/certifications/EDL.png';
+import certificado7 from '/images/certifications/Linux Cisco.png';
+import certificado8 from '/images/certifications/open robotics.png';
+import certificado9 from '/images/certifications/python profesional.png';
+import certificado10 from '/images/certifications/react-profesional.png';
+import certificado11 from '/images/certifications/redes.png';
+import certificado12 from '/images/certifications/ror.png';
+import certificado13 from '/images/certifications/rorDiploma.png';
+import certificado14 from '/images/certifications/desarrollo web profesional.png';
+import certificado15 from '/images/certifications/bootcamp-rails.png';
+import certificado16 from '/images/certifications/monitor_suena_san_juan.png';
 
 function Certifications() {
   const images = [certificado1, 
@@ -39,13 +33,7 @@ function Certifications() {
                   certificado13, 
                   certificado14,
                   certificado15, 
-                  certificado16, 
-                  certificado17,
-                  certificado18,
-                  certificado19,
-                  certificado20,
-                  certificado21,
-                  certificado22,];
+                  certificado16];
 
   return (
     <section className="certifications-container">
