1c9d21ef0979bfdeea9d45f7bf01bf7b6d64b283
MarceloM47 <morinigomarcelo40@gmail.com>
2023-12-20 17:45:39 -0300

    Update the projects section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -3,7 +3,7 @@ import Project from '../components/Project'
 
 const lms_bg = "/images/projects_image/lms-bg.png";
 const coffe_shop_bg = "/images/projects_image/coffe-shop-bg.png";
-const eventos_bg = "/images/projects_image/eventos-bg.png";
+const advice_bg = "/images/projects_image/advice-generator-bg.png"
 
 function Projects() {
 
@@ -15,8 +15,11 @@ function Projects() {
         </div>
         <p className='title_section'>My <span>Projects</span></p>
           <div className="projects">
-            <Project src={lms_bg} url="https://github.com/MarceloM47/Library-MS" alt='Fondo de una Task List en Ruby on Rails'/>
-            <Project src={eventos_bg} url="https://eventos.marcelom47.online/" alt='Fondo de un sitio web de invitaciones digitales'/>
+            <p>Sistema de gestión de bibliotecas hecho con RoR, Postgresql y Bootstrap</p>
+            <Project src={lms_bg} url="https://github.com/MarceloM47/Library-MS" alt='Fondo de un sistema de gestión de bibliotecas en Ruby on Rails'/>
+            <p>Generador de consejos con la API de "Advice Slip" hecho con React, Sass y Typescript</p>
+            <Project src={advice_bg} url="https://advice-generator-alce47.vercel.app/" alt='Fondo de un generador de consejos'/>
+            <p>Tienda de café hecho con Html, Css y Js</p>
             <Project src={coffe_shop_bg} url="https://coffee-shop-liard.vercel.app/" alt='Fondo de una Tienda de café'/>
           </div>
     </section>
