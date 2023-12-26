ec6822089fb359c0f3abbfc28a982853c0a1c98b
MarceloM47 <morinigomarcelo40@gmail.com>
2023-12-26 15:24:56 -0300

    Add new project and add new prop

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,15 +1,16 @@
 interface ProjectProps {
-    src: string;
-    alt: string;
-    url: string;
+  src: string;
+  alt: string;
+  url: string;
+  mobile: boolean;
 }
 
-function Project({src, alt, url}: ProjectProps){
+function Project({ src, alt, url, mobile }: ProjectProps) {
   return (
-    <div className="project">
-        <a href={url} target="_blank"><img src={src} alt={alt} /></a>
-    </div>
-  )
+      <div className={`project ${mobile ? 'project-mobile' : ''}`}>
+          <a href={url} target="_blank"><img src={src} alt={alt} /></a>
+      </div>
+  );
 }
 
-export default Project
+export default Project;
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -4,6 +4,8 @@ import Project from '../components/Project'
 const lms_bg = "/images/projects_image/lms-bg.png";
 const coffe_shop_bg = "/images/projects_image/coffe-shop-bg.png";
 const advice_bg = "/images/projects_image/advice-generator-bg.png"
+const pomodoro_bg = "/images/projects_image/pomodoro-bg.jpeg"
+
 
 function Projects() {
 
@@ -16,11 +18,13 @@ function Projects() {
         <p className='title_section'>My <span>Projects</span></p>
           <div className="projects">
             <p>Sistema de gestión de bibliotecas hecho con RoR, Postgresql y Bootstrap</p>
-            <Project src={lms_bg} url="https://github.com/MarceloM47/Library-MS" alt='Fondo de un sistema de gestión de bibliotecas en Ruby on Rails'/>
+            <Project src={lms_bg} url="https://github.com/MarceloM47/Library-MS" alt='Fondo de un sistema de gestión de bibliotecas en Ruby on Rails' mobile={false}/>
             <p>Generador de consejos con la API de "Advice Slip" hecho con React, Sass y Typescript</p>
-            <Project src={advice_bg} url="https://advice-generator-alce47.vercel.app/" alt='Fondo de un generador de consejos'/>
+            <Project src={advice_bg} url="https://advice-generator-alce47.vercel.app/" alt='Fondo de un generador de consejos' mobile={false}/>
             <p>Tienda de café hecho con Html, Css y Js</p>
-            <Project src={coffe_shop_bg} url="https://coffee-shop-liard.vercel.app/" alt='Fondo de una Tienda de café'/>
+            <Project src={coffe_shop_bg} url="https://coffee-shop-liard.vercel.app/" alt='Fondo de una Tienda de café' mobile={false}/>
+            <p>Aplicación de Pomodoro con React Native</p>
+            <Project src={pomodoro_bg} url="https://github.com/MarceloM47/pomodoro-react-native" alt='Fondo de una App de Pomodoro' mobile={true}/>
           </div>
     </section>
   )
