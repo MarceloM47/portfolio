54a98b2048120c8a7d2d2ee4870ddbea92b2bdec
MarceloM47 <morinigomarcelo40@gmail.com>
2023-12-30 14:55:11 -0300

    Add new project

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -3,7 +3,7 @@ import Project from '../components/Project'
 
 const lms_bg = "/images/projects_image/lms-bg.png";
 const coffe_shop_bg = "/images/projects_image/coffe-shop-bg.png";
-const advice_bg = "/images/projects_image/advice-generator-bg.png"
+const fake_store_bg = "/images/projects_image/fake-store-bg.png"
 const pomodoro_bg = "/images/projects_image/pomodoro-bg.jpeg"
 
 
@@ -19,8 +19,8 @@ function Projects() {
           <div className="projects">
             <p>Sistema de gestión de bibliotecas hecho con RoR, Postgresql y Bootstrap</p>
             <Project src={lms_bg} url="https://github.com/MarceloM47/Library-MS" alt='Fondo de un sistema de gestión de bibliotecas en Ruby on Rails' mobile={false}/>
-            <p>Generador de consejos con la API de "Advice Slip" hecho con React, Sass y Typescript</p>
-            <Project src={advice_bg} url="https://advice-generator-alce47.vercel.app/" alt='Fondo de un generador de consejos' mobile={false}/>
+            <p>Fake store hecha con React, React Router, React Query, Zustand y Boostrap</p>
+            <Project src={fake_store_bg} url="https://fake-store-api-alce47.vercel.app/" alt='Fondo de una fake store' mobile={false}/>
             <p>Tienda de café hecho con Html, Css y Js</p>
             <Project src={coffe_shop_bg} url="https://coffee-shop-liard.vercel.app/" alt='Fondo de una Tienda de café' mobile={false}/>
             <p>Aplicación de Pomodoro con React Native</p>
