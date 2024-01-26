129a505a0b39399c5ad216e06f5c04de23fcc6b5
MarceloM47 <morinigomarcelo40@gmail.com>
2024-01-26 17:45:55 -0300

    Update the Experience section

--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -10,15 +10,15 @@ function Experience() {
         <div className="exps-container">
             <div className="exp-container">
                 <small>Pasantía</small>
-                <h4>Business Software Solutions</h4>
-                <h5>Área de soporte</h5>
+                <p className="enterprise-name">Business Software Solutions</p>
+                <p className="area-name">Área de soporte</p>
                 <p>diciembre de 2022 - enero de 2023 (2 meses)</p>
                 <small>Caaguazú, Paraguay</small>
             </div>
             <div className="exp-container">
                 <small>Pasantía</small>
-                <h4>Entidad Binacional Yacyretá</h4>
-                <h5>Área de comunicaciones</h5>
+                <p className="enterprise-name">Entidad Binacional Yacyretá</p>
+                <p className="area-name">Área de comunicaciones</p>
                 <p>julio de 2023 - agosto de 2023 (2 meses)</p>
                 <small>Ayolas, Paraguay</small>
             </div>
