c95c8e4c4120c93d09034a953e4ac6057764019d
Marcelo Morinigo <78925750+MarceloM47@users.noreply.github.com>
2024-05-20 01:01:49 +0000

    Add Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css'
 import Menu from './sections/Menu'
 import Layout from './sections/Layout'
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
   return (
     <>
       <div className='app-container'>
+        <SpeedInsights/>
+        <Analytics/>
         <Menu/>
         <Layout/>
       </div>
