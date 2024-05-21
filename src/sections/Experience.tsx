dd8f19fee290971680d53c00199986e8b6a6d4de
Marcelo Morinigo <78925750+MarceloM47@users.noreply.github.com>
2024-05-21 00:40:53 +0000

    Add projects

--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -12,14 +12,14 @@ function Experience() {
                 <small>Pasantía</small>
                 <p className="enterprise-name">Business Software Solutions</p>
                 <p className="area-name">Área de soporte</p>
-                <p>diciembre de 2022 - enero de 2023 (2 meses)</p>
+                <p>diciembre de 2022 - enero de 2023</p>
                 <small>Caaguazú, Paraguay</small>
             </div>
             <div className="exp-container">
                 <small>Pasantía</small>
                 <p className="enterprise-name">Entidad Binacional Yacyretá</p>
                 <p className="area-name">Área de comunicaciones</p>
-                <p>julio de 2023 - agosto de 2023 (2 meses)</p>
+                <p>julio de 2023 - agosto de 2023</p>
                 <small>Ayolas, Paraguay</small>
             </div>
             <div className="exp-container">
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -6,7 +6,8 @@ const coffe_shop_bg = "/images/projects_image/coffe-shop-bg.png";
 const fake_store_bg = "/images/projects_image/fake-store-bg.png"
 const pomodoro_bg = "/images/projects_image/pomodoro-bg.jpeg"
 const fake_store_mobile_bg = "/images/projects_image/fake-store-mobile-bg.jpeg"
-
+const colacion_bg = "/images/projects_image/colacion-bg.png"
+const qr_app = "/images/projects_image/qr-app.png"
 
 function Projects() {
 
@@ -20,8 +21,10 @@ function Projects() {
           <div className="projects">
             <p>Sistema de gestión de bibliotecas hecho con RoR, Postgresql y Bootstrap</p>
             <Project src={lms_bg} url="https://github.com/MarceloM47/Library-MS" alt='Fondo de un sistema de gestión de bibliotecas en Ruby on Rails' mobile={false}/>
-            <p>Fake store hecha con React, React Router, React Query, Zustand y Bootstrap</p>
-            <Project src={fake_store_bg} url="https://fake-store-api-alce47.vercel.app/" alt='Fondo de una fake store' mobile={false}/>
+            <p>Página web de invitación hecha con Html, Css, Javascript, Bootstrap y Firebase</p>
+            <Project src={colacion_bg} url="https://marcelo-morinigo-colacion.vercel.app/" alt='Fondo de una invitación web a fiesta de colación' mobile={false}/>
+            <p>Página web de generación de códigos QR hecha con React, Typescript y Tailwindcss</p>
+            <Project src={qr_app} url="https://qr-code-generator-two-tau.vercel.app/" alt='Fondo de una página web de generación de códigos QR' mobile={false}/>
             <p>Tienda de café hecho con Html, Css y Javascript</p>
             <Project src={coffe_shop_bg} url="https://coffee-shop-liard.vercel.app/" alt='Fondo de una Tienda de café' mobile={false}/>
             <p>Aplicación de Pomodoro con React Native</p>
